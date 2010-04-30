ac2d26d255101aa1c2bf8aed2e72c0dba07ef126
Stian Soiland-Reyes <stian@soiland-reyes.com>
2010-04-30 18:11:53 +0100

    fully mavenized

---
+++ src/test/java/com/sun/media/imageio/ConverterTest.java
@@ -0,0 +1,37 @@
+package com.sun.media.imageio;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ConverterTest {
+
+	@Ignore
+	@Test
+	public void testname() throws Exception {
+		System.out.println(Arrays.asList(ImageIO.getReaderMIMETypes()));
+		System.out.println(Arrays.asList(ImageIO.getWriterFormatNames()));
+		System.out.println(Arrays.asList(ImageIO.getReaderFormatNames()));
+
+		URL pngFile = getClass().getResource("/test.png");
+		BufferedImage img = ImageIO.read(pngFile);
+
+		for (String type : ImageIO.getWriterFormatNames()) {
+			File f = File.createTempFile("imageio-test", "." + type);
+			ImageIO.write(img, type, f);
+			System.out.println(f);
+			try {
+				ImageIO.read(f);
+			} catch (Exception ex) {
+				System.err.println("Failed " + f  + " " + ex.getLocalizedMessage());
+			}
+		}
+	}
+	
+}
