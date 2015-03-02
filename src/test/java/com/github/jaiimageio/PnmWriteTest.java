545b5ce51569539c3ac647c99aedf3d540e3793f
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-03-02 11:57:12 +0000

    test with random picture

--- src/test/java/com/github/jaiimageio/PnmWriteTest.java
+++ src/test/java/com/github/jaiimageio/PnmWriteTest.java
@@ -6,13 +6,16 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Random;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriter;
 import javax.imageio.stream.ImageOutputStream;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -23,10 +26,22 @@ public class PnmWriteTest {
 
     private final BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB);
 
+    @Before
+    public void randomize() {
+    	Random r = new Random(1337);
+    	for (int x=0; x<16; x++) {
+    		for (int y=0; y<16; y++) {
+    			image.setRGB(x, y, r.nextInt());
+    		}
+		}
+    }
+    
+    
     @Test
     public void writeRaw() throws Exception {
         File f = File.createTempFile("test-raw", ".ppm");
         f.deleteOnExit();
+        //System.out.println(f);
         write(f, true, image);
     }
 
@@ -41,11 +56,11 @@ public class PnmWriteTest {
         Iterator<ImageWriter> writers = ImageIO.getImageWritersBySuffix("ppm");
         assertTrue(writers.hasNext());
         ImageWriter writer = writers.next();
-        PNMImageWriteParam defaultWriteParam = (PNMImageWriteParam) writer.getDefaultWriteParam();
-        defaultWriteParam.setRaw(raw);
+        PNMImageWriteParam params = (PNMImageWriteParam) writer.getDefaultWriteParam();
+        params.setRaw(raw);
         ImageOutputStream ios = ImageIO.createImageOutputStream(f);
         writer.setOutput(ios);
-        writer.write(null, new IIOImage(image, null, null), defaultWriteParam);
+        writer.write(null, new IIOImage(image, null, null), params);
         writer.dispose();
         ios.close();
     }
