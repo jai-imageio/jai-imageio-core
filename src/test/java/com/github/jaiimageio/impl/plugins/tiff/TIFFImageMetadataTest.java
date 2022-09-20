b9942c61782b4fbcef408ee845fc3663d25ae5ba
Nils Bifano <nils.bifano@tylertech.com>
2022-09-20 07:36:08 -0600

    TCM-7632 Add EXIFParentTIFFTagSet to supported class replacements

--- src/test/java/com/github/jaiimageio/impl/plugins/tiff/TIFFImageMetadataTest.java
+++ src/test/java/com/github/jaiimageio/impl/plugins/tiff/TIFFImageMetadataTest.java
@@ -2,6 +2,7 @@ package com.github.jaiimageio.impl.plugins.tiff;
 
 
 import com.github.jaiimageio.plugins.tiff.BaselineTIFFTagSet;
+import com.github.jaiimageio.plugins.tiff.EXIFParentTIFFTagSet;
 import com.github.jaiimageio.plugins.tiff.FaxTIFFTagSet;
 import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
 import org.junit.Test;
@@ -14,7 +15,19 @@ import javax.imageio.metadata.IIOInvalidTreeException;
 import static junit.framework.TestCase.assertEquals;
 
 public class TIFFImageMetadataTest {
-    
+
+    @Test
+    public void test_parseIFD_SUN_EXIFParentTIFFTagSetClassName() throws IIOInvalidTreeException {
+        Document xmlDoc = new DocumentImpl();
+        Element ifd = xmlDoc.createElement("TIFFIFD");
+        ifd.setAttribute("tagSets", TIFFImageMetadata.SUN_EXIFParentTIFFTagSetClassName);
+
+        TIFFIFD tiffIfd = TIFFImageMetadata.parseIFD(ifd);
+
+        assertEquals(1, tiffIfd.getTagSetList().size());
+        assertEquals(EXIFParentTIFFTagSet.class.getName(), tiffIfd.getTagSetList().get(0).getClass().getName());
+    }
+
     @Test
     public void test_parseIFD_SUN_FaxTIFFTagSetClassName() throws IIOInvalidTreeException {
         Document xmlDoc = new DocumentImpl();
